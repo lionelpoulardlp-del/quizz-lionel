893391024a1f47b666368909001c8bad4e8a7e25
lionelpoulardlp-del <lionelpoulard.lp@gmail.com>
2026-03-11 14:42:33 +0100

    Initialisation du projet quiz

--- src/main.js
+++ src/main.js
@@ -1,24 +1,124 @@
 import "./style.css";
 import quiz from "./quiz-femmes-scientifiques.json";
 
-const bouton = document.createElement("button");
+const app = document.querySelector("#app");
 
-bouton.textContent = "Start Quiz";
-bouton.id = "start-button";
+let currentQuestionIndex = 0;
+let score = 0;
 
-bouton.addEventListener("click", () => {
-  alert("Quiz Started!");
-  document.body.style.display = "none";
-});
+// écran accueil
+function renderStartScreen() {
+  app.innerHTML = `
+    <section class="screen">
+      <h1>${quiz.title}</h1>
+      <button id="start-button">Démarrer</button>
+    </section>
+  `;
 
-document.body.appendChild(bouton);
+  const startButton = document.querySelector("#start-button");
 
-const startScreen = document.createElement("div");
-startScreen.id = "start-screen";
-document.body.appendChild(startScreen);
+  startButton.addEventListener("click", () => {
+    renderQuestionScreen();
+  });
+}
 
-const header = document.createElement("header");
-const headerTitle = document.createElement("h1");
-headerTitle.textContent = quiz.title;
-header.appendChild(headerTitle);
-app.appendChild(header);
+// écran question
+function renderQuestionScreen() {
+  const question = quiz.questions[currentQuestionIndex];
+
+  app.innerHTML = `
+    <section class="screen">
+      <header>
+        <h1>${quiz.title}</h1>
+      </header>
+
+      <div>
+        <h2>${question.question}</h2>
+      </div>
+
+      <div class="answers" id="answers"></div>
+
+      <div class="message" id="message"></div>
+
+      <button id="next-button" class="hidden">Question suivante</button>
+    </section>
+  `;
+
+  const answersContainer = document.querySelector("#answers");
+  const messageDiv = document.querySelector("#message");
+  const nextButton = document.querySelector("#next-button");
+
+  question.options.forEach((option, index) => {
+    const answerButton = document.createElement("button");
+    answerButton.innerText = option;
+
+    answerButton.addEventListener("click", () => {
+      const allButtons = answersContainer.querySelectorAll("button");
+
+      allButtons.forEach((button) => {
+        button.disabled = true;
+      });
+
+      if (index === question.correctIndex) {
+        messageDiv.innerText = "Bonne réponse";
+        score++;
+      } else {
+        messageDiv.innerText = `Mauvaise réponse, la bonne réponse est ${question.options[question.correctIndex]}.`;
+      }
+
+      nextButton.classList.remove("hidden");
+    });
+
+    answersContainer.appendChild(answerButton);
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < quiz.questions.length) {
+      renderQuestionScreen();
+    } else {
+      renderEndScreen();
+    }
+  });
+}
+
+// écran fin
+function renderEndScreen() {
+  const totalQuestions = quiz.questions.length;
+  const percentage = (score / totalQuestions) * 100;
+
+  let endTitle = "";
+
+  if (percentage === 0) {
+    endTitle = "Oups ! Tu n'as trouvé aucune bonne réponse 😱";
+  } else if (percentage < 50) {
+    endTitle = "Aïe, tu as beaucoup d'erreurs, tu devrais réessayer 😅";
+  } else if (percentage < 80) {
+    endTitle = "C'est pas mal, mais tu peux encore t'améliorer 💪";
+  } else if (percentage < 100) {
+    endTitle = "C'est bien, tu as fait peu d'erreurs 😉";
+  } else {
+    endTitle = "Aucune erreur, c'est parfait 😎";
+  }
+
+  app.innerHTML = `
+    <section class="screen">
+      <h1>${endTitle}</h1>
+      <h2>Ton score est de</h2>
+      <div class="score">${score} / ${totalQuestions}</div>
+      <button id="restart-button">Recommencer le quiz</button>
+    </section>
+  `;
+
+  const restartButton = document.querySelector("#restart-button");
+
+  restartButton.addEventListener("click", () => {
+    currentQuestionIndex = 0;
+    score = 0;
+    renderStartScreen();
+  });
+}
+
+// lancement
+renderStartScreen();
